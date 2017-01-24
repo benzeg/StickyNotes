a133c0238961b74eb330202d758e1bdde6530347
Hack Reactor Students <accounts@hackreactor.com>
2017-01-23 23:25:33 -0800

    added some changes

--- app/models/user.js
+++ app/models/user.js
@@ -6,4 +6,11 @@ var mongoose = require('mongoose');
 
 var User = mongoose.model('User', db.users);
 
+User.comparePassword = function(candidatePassword, savedPassword, cb) {
+  bcrypt.compare(candidatePassword, savedPassword, function(err, isMatch) {
+    if (err) { return cb(err); }
+    cb(null, isMatch);
+  });
+};
+
 module.exports = User;
